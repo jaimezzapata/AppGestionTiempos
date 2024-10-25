225c342b579229b5a52d0af3558a13a6c7e53355
jaimezzapata <zapataval2304@gmail.com>
2024-10-24 20:36:43 -0500

    Se crea estructura básica de viewProyectos

--- js/validator.js
+++ js/validator.js
@@ -1,3 +1,4 @@
 import { registrarTiempos } from "../controllers/ControllerTiempos.js";
+import { listarProyectos, filtrarPorEmpresa, filtrarPorProyecto, filtrarPorTipoProyecto } from '../controllers/ControllerProyectos.js'
 
 document.getElementById('btnRegistrar').addEventListener('click', registrarTiempos)
---
+++ models/ModelProyectos.js
@@ -0,0 +1,22 @@
+export const proyectos = [
+    { nombre: 'Proyecto A', duracion: 6, empresa: 'Empresa 1', tipo: 'front' },
+    { nombre: 'Proyecto B', duracion: 12, empresa: 'Empresa 2', tipo: 'back' },
+    { nombre: 'Proyecto C', duracion: 8, empresa: 'Empresa 3', tipo: 'full stack' },
+    { nombre: 'Proyecto D', duracion: 3, empresa: 'Empresa 4', tipo: 'front' },
+    { nombre: 'Proyecto E', duracion: 9, empresa: 'Empresa 5', tipo: 'back' },
+    { nombre: 'Proyecto F', duracion: 5, empresa: 'Empresa 6', tipo: 'front' },
+    { nombre: 'Proyecto G', duracion: 10, empresa: 'Empresa 7', tipo: 'full stack' },
+    { nombre: 'Proyecto H', duracion: 7, empresa: 'Empresa 8', tipo: 'back' },
+    { nombre: 'Proyecto I', duracion: 4, empresa: 'Empresa 9', tipo: 'front' },
+    { nombre: 'Proyecto J', duracion: 11, empresa: 'Empresa 10', tipo: 'full stack' },
+    { nombre: 'Proyecto K', duracion: 6, empresa: 'Empresa 11', tipo: 'back' },
+    { nombre: 'Proyecto L', duracion: 9, empresa: 'Empresa 12', tipo: 'front' },
+    { nombre: 'Proyecto M', duracion: 8, empresa: 'Empresa 13', tipo: 'full stack' },
+    { nombre: 'Proyecto N', duracion: 5, empresa: 'Empresa 14', tipo: 'back' },
+    { nombre: 'Proyecto O', duracion: 10, empresa: 'Empresa 15', tipo: 'front' },
+    { nombre: 'Proyecto P', duracion: 7, empresa: 'Empresa 16', tipo: 'full stack' },
+    { nombre: 'Proyecto Q', duracion: 12, empresa: 'Empresa 17', tipo: 'back' },
+    { nombre: 'Proyecto R', duracion: 3, empresa: 'Empresa 18', tipo: 'front' },
+    { nombre: 'Proyecto S', duracion: 4, empresa: 'Empresa 19', tipo: 'full stack' },
+    { nombre: 'Proyecto T', duracion: 9, empresa: 'Empresa 20', tipo: 'back' }
+];
