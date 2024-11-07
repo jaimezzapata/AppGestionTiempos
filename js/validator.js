7da0e71d882b0b45c3a0363357fbec4ed7b4caa2
jaimezzapata <zapataval2304@gmail.com>
2024-11-07 12:31:02 -0500

    Modificación de estilos y contenido del arreglo

--- js/validator.js
+++ js/validator.js
@@ -1,4 +1,11 @@
 import { registrarTiempos } from "../controllers/ControllerTiempos.js";
-import { listarProyectos, filtrarPorEmpresa, filtrarPorProyecto, filtrarPorTipoProyecto } from '../controllers/ControllerProyectos.js'
+import {
+  listarProyectos,
+  filtrarPorEmpresa,
+  filtrarPorProyecto,
+  filtrarPorTipoProyecto,
+} from "../controllers/ControllerProyectos.js";
 
-document.getElementById('btnRegistrar').addEventListener('click', registrarTiempos)
+document
+  .getElementById("btnRegistrar")
+  .addEventListener("click", registrarTiempos);
